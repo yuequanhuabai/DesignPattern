0e7da8329958839afd65034ec7897945087ea651
yq <yuequanhuabai@qq.com>
2021-07-22 20:58:22 +0800

    strategy model commit

---
+++ src/main/java/com/design/pattern/Strategy.java
@@ -0,0 +1,14 @@
+package com.design.pattern;
+
+/**
+ * @ClassName Strategy
+ * @Description 描述
+ * @Author YuequanHuabai
+ * @Date 2021/7/22 20:57
+ * @Version 1.0
+ **/
+public class Strategy {
+    public static void main(String[] args) {
+        System.out.println("strategy model");
+    }
+}
