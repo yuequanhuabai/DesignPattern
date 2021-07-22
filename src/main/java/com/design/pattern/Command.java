8638080aeed71a81a2d8c8d508fa98860f77f894
yq <yuequanhuabai@qq.com>
2021-07-22 20:47:58 +0800

    first commit

---
+++ src/main/java/com/design/pattern/Command.java
@@ -0,0 +1,15 @@
+package com.design.pattern;
+
+/**
+ * @ClassName Command
+ * @Description 描述
+ * @Author YuequanHuabai
+ * @Date 2021/7/22 20:42
+ * @Version 1.0
+ **/
+public class Command {
+    public static void main(String[] args) {
+
+        System.out.println("hello world!");
+    }
+}
